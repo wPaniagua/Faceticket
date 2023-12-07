c3a99d3e2c37b2f918a7501a6dd52dd4c1a452da
Wilson <13283998+wPaniagua@users.noreply.github.com>
2023-12-06 23:37:15 -0600

    Add files via upload

---
+++ Includes/templates/footer.php
@@ -0,0 +1,27 @@
+		<footer class="footer_section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 xs-padding">
+						<div class="copyright">
+							<h3>FaceTiket</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+		<!-- INCLUDE JS SCRIPTS -->
+
+		<script src="Design/js/jquery.min.js"></script>
+		<script src="Design/js/bootstrap.min.js"></script>
+		<script src="Design/js/bootstrap.bundle.min.js"></script>
+		<script src="Design/js/main.js"></script>
+
+		</body>
+
+		<!-- END BODY TAG -->
+
+		</html>
+
+		<!-- END HTML TAG -->
